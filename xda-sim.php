adb649abd0959fa04bc8b1902903722ae2377d1b
Joe Maples <joe@frap129.org>
2017-09-06 09:31:12 -0400

    Create xda-sim.php

---
+++ xda-sim.php
@@ -0,0 +1,48 @@
+<?php
+/*
+REQUIREMENTS
+* A custom slash command on a Slack team
+* A web server running PHP5
+USAGE
+* Place this script on a server running PHP5 with cURL.
+* Set up a new custom slash command on your Slack team: 
+  http://my.slack.com/services/new/slash-commands
+* Under "Choose a command", enter whatever you want for 
+  the command. /xda is easy to remember.
+* Under "URL", enter the URL for the script on your server.
+* Leave "Method" set to "Post".
+* Decide whether you want this command to show in the 
+  autocomplete list for slash commands.
+* If you do, enter a short description and usage hint.
+*/
+# Grab some of the values from the slash command, create vars for post back to Slack
+$command = $_POST['command'];
+$token = $_POST['token'];
+# Check the token and make sure the request is from our team 
+if($token != 'S13kr23oqMAUvTaLn0cD1oDe'){ #replace this with the token from your slash command configuration page
+  $msg = "The token for the slash command doesn't match. Check your script.";
+  die($msg);
+  echo $msg;
+}
+$user_agent = "XDASimulator/1.0";
+$words = array( "voltez", "does", "no", "not", "thinks", "dev", "sir", "pls",
+                "jiosim", "rom", "bro", "carbin", "kernal", "voLte", "broke",
+                "work", "good", "bad", "neeed", "it", "its", "other" );
+$size = rand(2, 10);
+$reply = "";
+for ($i = 0; $i < $size; $i++) {
+	$reply .= $words[array_rand($words, 1)];
+	$reply .= " ";
+}
+
+# Encode response in JSON
+$data = array(
+	response_type => in_channel,
+  text => $reply
+);
+
+header('Content-type:application/json');
+$message = json_encode($data);
+
+# Send the reply back to the user. 
+echo $message;
